37780b92601c6171935f8bce0aed9eb6dd214265
Marvin Reimer <therealmarv@gmail.com>
2014-08-06 18:07:37 +0200

    Optimize Vagrant installation
    Do not use virtualenv for vagrant.
    Point to Github Locust master version which works better for non virtualenv setups.
    Updated readme for Vagrant usage.

--- vagrant.sh
+++ vagrant.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -ex
 apt-get update -y
-apt-get install -y python-dev python-pip python-virtualenv
+apt-get install -y python-dev python-pip
 # optimize TCP/IP settings
 sysctl -w net.core.somaxconn=10000
 sysctl -w net.ipv4.tcp_max_syn_backlog=10000
-# install virtualenv... TODO
-# virtualenv /home/vagrant/virtenv
-# source /home/vagrant/virtenv/bin/activate
-# pip install locustio
+cd /vagrant
+pip install -r requirements.txt
