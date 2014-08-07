3d56f657ccd9521b20e42a661c284fe56132269d
Marvin Reimer <therealmarv@gmail.com>
2014-08-07 15:30:09 +0200

    install also git-core

--- vagrant.sh
+++ vagrant.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 apt-get update -y
-apt-get install -y python-dev python-pip
+apt-get install -y python-dev python-pip git-core
 # optimize TCP/IP settings
 sysctl -w net.core.somaxconn=10000
 sysctl -w net.ipv4.tcp_max_syn_backlog=10000
